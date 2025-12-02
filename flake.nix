db4da74e973ba50c2330f2b3838ff59b4bc0e0a1
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-12-02 20:08:25 +0100

    build: Setup dev environment

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Flake for tree-sitter-lilypond";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    inherit (nixpkgs) lib;
+    forEachSystem = lib.genAttrs lib.systems.flakeExposed;
+  in {
+    devShells = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        name = "tree-sitter-lilypond";
+        packages = [
+          pkgs.gcc
+          pkgs.nodejs-slim
+          pkgs.prettierd
+          pkgs.tree-sitter
+          pkgs.typescript-language-server
+          pkgs.typos
+        ];
+      };
+    });
+  };
+}
