6f211910f749a5c5fe7854018695aa367921e669
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-12-02 20:22:10 +0100

    chore: npm install

---
+++ build/Release/.deps/Release/obj.target/tree_sitter_lilypond_binding/bindings/node/binding.o.d
@@ -0,0 +1,19 @@
+cmd_Release/obj.target/tree_sitter_lilypond_binding/bindings/node/binding.o := g++ -o Release/obj.target/tree_sitter_lilypond_binding/bindings/node/binding.o ../bindings/node/binding.cc '-DNODE_GYP_MODULE_NAME=tree_sitter_lilypond_binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_FILE_OFFSET_BITS=64' '-D_LARGEFILE_SOURCE' '-D__STDC_FORMAT_MACROS' '-DNAPI_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/home/kuni/.cache/node-gyp/22.21.1/include/node -I/home/kuni/.cache/node-gyp/22.21.1/src -I/home/kuni/.cache/node-gyp/22.21.1/deps/openssl/config -I/home/kuni/.cache/node-gyp/22.21.1/deps/openssl/openssl/include -I/home/kuni/.cache/node-gyp/22.21.1/deps/uv/include -I/home/kuni/.cache/node-gyp/22.21.1/deps/zlib -I/home/kuni/.cache/node-gyp/22.21.1/deps/v8/include -I../src -I../node_modules/node-addon-api  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -fPIC -m64 -O3 -flto=4 -fuse-linker-plugin -ffat-lto-objects -fno-omit-frame-pointer -fno-rtti -fno-strict-aliasing -std=gnu++17 -MMD -MF ./Release/.deps/Release/obj.target/tree_sitter_lilypond_binding/bindings/node/binding.o.d.raw   -c
+Release/obj.target/tree_sitter_lilypond_binding/bindings/node/binding.o: \
+ ../bindings/node/binding.cc ../node_modules/node-addon-api/napi.h \
+ /home/kuni/.cache/node-gyp/22.21.1/include/node/node_api.h \
+ /home/kuni/.cache/node-gyp/22.21.1/include/node/js_native_api.h \
+ /home/kuni/.cache/node-gyp/22.21.1/include/node/js_native_api_types.h \
+ /home/kuni/.cache/node-gyp/22.21.1/include/node/node_api_types.h \
+ ../node_modules/node-addon-api/napi-inl.h \
+ ../node_modules/node-addon-api/napi.h \
+ ../node_modules/node-addon-api/napi-inl.deprecated.h
+../bindings/node/binding.cc:
+../node_modules/node-addon-api/napi.h:
+/home/kuni/.cache/node-gyp/22.21.1/include/node/node_api.h:
+/home/kuni/.cache/node-gyp/22.21.1/include/node/js_native_api.h:
+/home/kuni/.cache/node-gyp/22.21.1/include/node/js_native_api_types.h:
+/home/kuni/.cache/node-gyp/22.21.1/include/node/node_api_types.h:
+../node_modules/node-addon-api/napi-inl.h:
+../node_modules/node-addon-api/napi.h:
+../node_modules/node-addon-api/napi-inl.deprecated.h:
